34ee03cf24db3eb68244b859afe3aea73239cd63
lumiByeol <julie.karakulova@gmail.com>
2023-04-25 21:30:42 +0300

    commit attempt 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.ArrayList;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Student> studentMarks = new ArrayList<>();
+        studentMarks.add(new Student("Иванов", 4.5));
+        studentMarks.add(new Student("Петров", 3.7));
+        studentMarks.add(new Student("Сидоров", 4.2));
+        studentMarks.add(new Student("Козлов", 5.0));
+        studentMarks.add(new Student("Горелов", 4.9));
+        studentMarks.add(new Student("Вятский", 3.75));
+        studentMarks.add(new Student("Орлов", 3.1));
+        studentMarks.add(new Student("Магаданов", 4.23));
+
+        for (Student studentMark : studentMarks) {
+            System.out.println(studentMark.getName() + " " + studentMark.getMark());
+        }
+
+        Student empty;
+
+        System.out.println("-------------");
+
+        for (int i=0; i < (studentMarks.size()-1); i++){
+            for (int j=0; j < (studentMarks.size()-1); j++) {
+                if ((studentMarks.get(j)).getMark() > (studentMarks.get(j + 1)).getMark()) {
+                    empty = studentMarks.get(j);
+                    studentMarks.set(j, studentMarks.get(j + 1));
+                    studentMarks.set(j + 1, empty);
+                }
+            }
+        }
+
+        for (Student studentMark : studentMarks) {
+            System.out.println(studentMark.getName() + " " + studentMark.getMark());
+        }
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    String name;
+    double mark;
+
+    public Student(String name, double mark){
+        this.name = name;
+        this.mark = mark;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public double getMark(){
+        return mark;
+    }
 }
