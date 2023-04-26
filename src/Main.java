f2ae5d0fa2e2d98df63d0d5e4a7319d7a920543c
lumiByeol <julie.karakulova@gmail.com>
2023-04-26 20:03:07 +0300

    made bubblesort a method

--- src/Main.java
+++ src/Main.java
@@ -2,36 +2,40 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
-        ArrayList<Student> studentMarks = new ArrayList<>();
-        studentMarks.add(new Student("Иванов", 4.5));
-        studentMarks.add(new Student("Петров", 3.7));
-        studentMarks.add(new Student("Сидоров", 4.2));
-        studentMarks.add(new Student("Козлов", 5.0));
-        studentMarks.add(new Student("Горелов", 4.9));
-        studentMarks.add(new Student("Вятский", 3.75));
-        studentMarks.add(new Student("Орлов", 3.1));
-        studentMarks.add(new Student("Магаданов", 4.23));
-
-        for (Student studentMark : studentMarks) {
-            System.out.println(studentMark.getName() + " " + studentMark.getMark());
-        }
-
+    public static void bubblesort(ArrayList<Student> arrList) {
         Student empty;
 
-        System.out.println("-------------");
-
-        for (int i=0; i < (studentMarks.size()-1); i++){
-            for (int j=0; j < (studentMarks.size()-1); j++) {
-                if ((studentMarks.get(j)).getMark() > (studentMarks.get(j + 1)).getMark()) {
-                    empty = studentMarks.get(j);
-                    studentMarks.set(j, studentMarks.get(j + 1));
-                    studentMarks.set(j + 1, empty);
+        for (int i = 0; i < arrList.size() - 1; i++) {
+            for (int j = 0; j < (arrList.size() - 1); j++) {
+                if ((arrList.get(j)).getMark() > (arrList.get(j + 1)).getMark()) {
+                    empty = arrList.get(j);
+                    arrList.set(j, arrList.get(j + 1));
+                    arrList.set(j + 1, empty);
                 }
             }
         }
+    }
 
-        for (Student studentMark : studentMarks) {
+    public static void main(String[] args) {
+        ArrayList<Student> studentList = new ArrayList<>();
+        studentList.add(new Student("Иванов", 4.5));
+        studentList.add(new Student("Петров", 3.7));
+        studentList.add(new Student("Сидоров", 4.2));
+        studentList.add(new Student("Козлов", 5.0));
+        studentList.add(new Student("Горелов", 4.9));
+        studentList.add(new Student("Вятский", 3.75));
+        studentList.add(new Student("Орлов", 3.1));
+        studentList.add(new Student("Магаданов", 4.23));
+
+        for (Student studentMark : studentList) {
+            System.out.println(studentMark.getName() + " " + studentMark.getMark());
+        }
+
+        System.out.println("-------------");
+
+        bubblesort(studentList);
+
+        for (Student studentMark : studentList) {
             System.out.println(studentMark.getName() + " " + studentMark.getMark());
         }
     }
